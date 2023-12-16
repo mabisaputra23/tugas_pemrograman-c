d254a71f75ca85a7a142f0e7a4faf45fc92aa67d
unknown <rifkiuye75@gmail.com>
2023-12-16 19:13:03 +0700

    first commit

---
+++ tugas 6/tugas 1 pertemuan 6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Mencetak deret angka 10 hingga 100 dengan selisih 10
+    int i;
+    for ( i = 10; i <= 100; i += 10) {
+        printf("%d ", i);
+    }
+
+    printf("\n"); // Menambahkan baris baru setelah mencetak deret angka
+
+    return 0;
+}
+
---
+++ tugas 6/tugas 2 pertemuan 6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+		// follow rafi yah
+    // Mencetak deret angka 100 hingga 55 dengan selisih 5
+    int i;
+    for (i = 100; i >= 55; i -= 5) {
+        printf("%d ", i);
+    }
+
+    printf("\n"); // Menambahkan baris baru setelah mencetak deret angka
+
+    return 0;
+}
+
